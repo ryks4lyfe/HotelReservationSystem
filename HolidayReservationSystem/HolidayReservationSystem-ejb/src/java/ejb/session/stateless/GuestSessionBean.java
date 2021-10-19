b8b7053c0291ddc610d36d8fd4dd199d19aa698c
ryks4lyfe <77714726+ryks4lyfe@users.noreply.github.com>
2021-10-19 13:49:22 +0800

    Add files via upload

---
+++ HolidayReservationSystem/HolidayReservationSystem-ejb/src/java/ejb/session/stateless/GuestSessionBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author 65912
+ */
+@Stateless
+public class GuestSessionBean implements GuestSessionBeanRemote, GuestSessionBeanLocal {
+
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ HolidayReservationSystem/HolidayReservationSystemLibrary/src/entity/Guest.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author 65912
+ */
+@Entity
+public class Guest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long guestId;
+    
+    @Column(nullable = false, length = 64)
+    private String name;
+    @Column(nullable = false, unique = true, length = 32)
+    private String email;
+    @Column(nullable = false, length = 32)
+    private String password;
+    @Column(nullable = false, length = 32)
+    private String phoneNum;
+    @Column(nullable = false, length = 32)
+    private String passportNum;
+    
+    //@OneToMany(mappedBy = "guest")
+    //private List<OnlineReservation> onlineReservations;
+    
+    public Guest() {
+        //onlineReservations = new ArrayList<>();
+    }
+
+    public Guest(String name, String email, String password, String phoneNum, String passportNum) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNum = phoneNum;
+        this.passportNum = passportNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getPassportNum() {
+        return passportNum;
+    }
+
+    public void setPassportNum(String passportNum) {
+        this.passportNum = passportNum;
+    }
+
+    //public List<OnlineReservation> getOnlineReservations() {
+        //return onlineReservations;
+    //}
+
+    //public void setOnlineReservations(List<OnlineReservation> onlineReservations) {
+       // this.onlineReservations = onlineReservations;
+    //}
+    
+    
+    
+
+    public Long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(Long guestId) {
+        this.guestId = guestId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (guestId != null ? guestId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the guestId fields are not set
+        if (!(object instanceof Guest)) {
+            return false;
+        }
+        Guest other = (Guest) object;
+        if ((this.guestId == null && other.guestId != null) || (this.guestId != null && !this.guestId.equals(other.guestId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Guest[ id=" + guestId + " ]";
+    }
+    
+}
