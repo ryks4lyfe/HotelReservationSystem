6dfd0bc86cf7e85f1ff0d282b5e674a1f8bfef55
ryks4lyfe <rykerynwa@gmail.com>
2021-10-19 14:02:23 +0800

    Added all my entities

---
+++ HolidayReservationSystem/HolidayReservationSystemLibrary/src/util/enumeration/EmployeeAccessRight.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.enumeration;
+
+/**
+ *
+ * @author 65912
+ */
+public enum EmployeeAccessRight {
+    SYSTEM_ADMINISTRATOR,
+    OPERATION_MANAGER,
+    SALES_MANAGER,
+    GUEST_RELATION_MANAGER
+}
+
